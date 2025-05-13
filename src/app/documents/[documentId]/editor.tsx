455eb88e620564505b3214baf3059b8bf6cb970b
BenjaminChien <ben19990616@gmail.com>
2025-05-13 15:38:11 +0800

    Feature color and highlight added

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -12,6 +12,8 @@ import Image from '@tiptap/extension-image'
 import ImageResize from 'tiptap-extension-resize-image'
 import FontFamily from '@tiptap/extension-font-family'
 import TextStyle from '@tiptap/extension-text-style'
+import {Color} from '@tiptap/extension-color'
+import Highlight from '@tiptap/extension-Highlight'
 import { useEditor, EditorContent } from '@tiptap/react'
 
 import { useEditorStore } from '@/store/use-editor-store';
@@ -52,6 +54,10 @@ export const Editor = () =>{
         },
         extensions: [
             StarterKit,
+            Color,
+            Highlight.configure({
+                multicolor: true,
+            }),
             TextStyle,
             FontFamily,
             Underline,
